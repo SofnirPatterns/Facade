9bea6697f2eac3db87a7f9148f6c2a579be76c4d
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2020-04-17 20:57:33 +0200

    Creating classes for video downloader facade

---
+++ Facade/Video.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class Video
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string URL { get; set; }
+    }
+}
---
+++ Facade/VideoLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facade
+{
+    public class VideoLibrary
+    {
+        List<Video> Videos = new List<Video>()
+        {
+            new Video() { Id = 1, Title = "Harry Portier", URL = "https://www.youtube.com/watch?v=Htaj3o3JD8I" },
+            new Video() { Id = 2, Title = "Denver kumpel sprzed 6 tys. lat", URL = "https://vimeo.com/84114639" }
+        };
+
+        public Video GetVideo(string title)
+        {
+            return Videos.Where(q => String.Equals(q.Title, title, StringComparison.CurrentCultureIgnoreCase))?.FirstOrDefault()
+                ?? new Video() { Id = 0, Title = "Not found", URL = String.Empty };
+        }
+    }
+}
