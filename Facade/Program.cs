852f774964c1098c1040da4ce6cad56c7bfab721
macie <macie@DESKTOP-GUK2RJ2>
2021-04-11 20:43:21 +0200

    run on vs 2019

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -7,7 +7,7 @@ namespace Facade
         static void Main(string[] args)
         {
             VideoDownloader videoDownloader = new VideoDownloader();
-            string file = videoDownloader.Download(3, Enums.VideoService.YouTube, Enums.FileType.Ogg);
+            string file = videoDownloader.Download(2, Enums.VideoService.Vimeo, Enums.FileType.MPEG4);
             Console.WriteLine(file);
             Console.ReadKey();
         }
